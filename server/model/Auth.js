47c483a9d5aaa3f7b0a9e30b9fd31589554f5282
parth-chovatiya <parthchovatiya3@gmail.com>
2022-07-25 16:47:32 +0530

    Auth model changes

---
+++ server/model/Auth.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+
+const authSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    hashed_password: {
+      type: String,
+      required: true,
+    },
+    salt: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+authSchema
+  .virtual("password")
+  .set(function (password) {
+    //create a temporarity variable called password
+    this._password = password;
+    //gererate salt
+    this.salt = this.makeSalt();
+    //encryptPassword
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+  authSchema.methods = {
+    authenticate: function (plainText) {
+      return this.encryptPassword(plainText) === this.hashed_password;
+    },
+  
+    encryptPassword: function (password) {
+      if (!password) return "";
+      try {
+        return crypto
+          .createHmac("sha1", this.salt)
+          .update(password)
+          .digest("hex");
+      } catch (err) {
+        return "";
+      }
+    },
+  
+    makeSalt: function () {
+      return Math.round(new Date().valueOf() * Math.random()) + "";
+    },
+  };
+
+module.exports = mongoose.model("Auth", authSchema);
